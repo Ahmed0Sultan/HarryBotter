9046c42b57a7fd68ee0aed8f8da4e8d57f3e562a
Ahmed Sultan <ahmed.khaled4941@ci.menofia.edu.eg>
2017-03-03 16:59:21 +0200

    Verfiy Token Updated!!

--- HarryBotter.py
+++ HarryBotter.py
@@ -1,50 +1,57 @@
-from flask import Flask,request,json
-import os,sys
+import os
+import sys
+import json
+
 import requests
+from flask import Flask, request
+
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/', methods=['GET'])
+def verify():
+    # when the endpoint is registered as a webhook, it must echo back
+    # the 'hub.challenge' value it receives in the query arguments
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == os.environ["Z7Ylb5tyL5JR9EFYWHdw"]:
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
 
-@app.route('/webhook', methods=['GET', 'POST'])
+    return "Hello world", 200
+
+
+@app.route('/', methods=['POST'])
 def webhook():
-    if request.method == 'GET':
-        if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-            if not request.args.get("hub.verify_token") == os.environ["Z7Ylb5tyL5JR9EFYWHdw"]:
-                return "Verification token mismatch", 403
-            return request.args["hub.challenge"], 200
-    elif request.method == 'POST':
-        data = request.get_json()
-        log(data)  # you may not want to log every incoming message in production, but it's good for testing
 
-        if data["object"] == "page":
+    # endpoint for processing incoming messaging events
 
-            for entry in data["entry"]:
-                for messaging_event in entry["messaging"]:
+    data = request.get_json()
+    log(data)  # you may not want to log every incoming message in production, but it's good for testing
 
-                    if messaging_event.get("message"):  # someone sent us a message
+    if data["object"] == "page":
 
-                        sender_id = messaging_event["sender"][
-                            "id"]  # the facebook ID of the person sending you the message
-                        recipient_id = messaging_event["recipient"][
-                            "id"]  # the recipient's ID, which should be your page's facebook ID
-                        message_text = messaging_event["message"]["text"]  # the message's text
+        for entry in data["entry"]:
+            for messaging_event in entry["messaging"]:
 
-                        send_message(sender_id, "got it, thanks!")
+                if messaging_event.get("message"):  # someone sent us a message
 
-                    if messaging_event.get("delivery"):  # delivery confirmation
-                        pass
+                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                    message_text = messaging_event["message"]["text"]  # the message's text
 
-                    if messaging_event.get("optin"):  # optin confirmation
-                        pass
+                    send_message(sender_id, "got it, thanks!")
 
-                    if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
-                        pass
+                if messaging_event.get("delivery"):  # delivery confirmation
+                    pass
 
-        return "ok", 200
-    return "WebHooked"
+                if messaging_event.get("optin"):  # optin confirmation
+                    pass
+
+                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
+                    pass
+
+    return "ok", 200
+
 
 def send_message(recipient_id, message_text):
 
@@ -69,9 +76,11 @@ def send_message(recipient_id, message_text):
         log(r.status_code)
         log(r.text)
 
+
 def log(message):  # simple wrapper for logging to stdout on heroku
     print str(message)
     sys.stdout.flush()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
