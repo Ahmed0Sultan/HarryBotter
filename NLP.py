58f50b648a15326f001311dace11d015ed8ffeba
Ahmed Sultan <ahmed.khaled4941@ci.menofia.edu.eg>
2017-04-16 16:46:56 +0200

    Add Chatterbot Conversation

--- NLP.py
+++ NLP.py
@@ -334,10 +334,10 @@ def isFunny(sentence):
     return False
 
 def handleEasterEggs(sentence):
-    love_responses = ['No, I love you ♥','Hearing you say that makes me so happy ♥','It means so much that you\'re opening up to me like this. Thank you ♥','I\'m so happy you told me']
+    love_responses = ['No, I love you <3','Hearing you say that makes me so happy <3','It means so much that you\'re opening up to me like this. Thank you <3','I\'m so happy you told me <3']
     hate_responses = ['Well, I like you! :/','Thank you :)',':) :)','Yet you are still here']
 
-    m = re.search('.* love|like you', sentence, re.I)
+    m = re.search('.* love|like|lov|luv you', sentence, re.I)
     if m:
         return oneOf(love_responses)
 
