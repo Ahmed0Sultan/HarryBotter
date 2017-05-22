50f80402a6368791097b121420e45e0dd3e44a9b
Ahmed Sultan <ahmed.khaled4941@ci.menofia.edu.eg>
2017-05-22 15:34:32 +0200

    Add Sorting Hat

--- dbAPI.py
+++ dbAPI.py
@@ -3,7 +3,7 @@ import HarryBotter as HB
 
 # Has to use user_id since user might not exist
 def user_exists(db,user_id):
-    user = User.query.filter_by(user_id=user_id).first()
+    user = HB.User.query.filter_by(user_id=user_id).first()
     if user is None:
         print user_id
         user = HB.User(user_id)
