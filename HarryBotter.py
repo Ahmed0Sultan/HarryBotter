aef58811f74ae691f01b1205739fd5d8d96ba519
Ahmed Sultan <ahmed.khaled4941@ci.menofia.edu.eg>
2017-05-27 15:57:12 +0200

    Add LeaderBoard

--- HarryBotter.py
+++ HarryBotter.py
@@ -1994,10 +1994,13 @@ def handleShare(db,user_id,sender_id):
 
 def handleLeaderBoard(db,user_id):
     users = User.query.all()
+    first = users[0]
     first.points = 0
     print str(first)
+    second = users[0]
     second.points = 0
     print str(second)
+    third = users[0]
     third.points = 0
     print str(third)
     for user in users:
