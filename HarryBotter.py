62ab144f1c995466038daf4bfcd374861afa0879
Ahmed Sultan <ahmed.khaled4941@ci.menofia.edu.eg>
2017-05-31 03:27:09 +0200

    Add Point Reset

--- HarryBotter.py
+++ HarryBotter.py
@@ -269,7 +269,7 @@ NO_INFORMATION_AVAILABLE = 'Even \"Hogwarts: A History\" couldn\'t answer that q
 RESPONSE_STARTERS = ['', 'Well, ' 'You see, ', 'I know that ', 'I believe that ', 'It is said that ',
                      'To my knowledge, ']
 dbAPI.addHouses(db)
-dbAPI.resetPoints(db)
+# dbAPI.resetPoints(db)
 @app.route('/privacy', methods=['GET'])
 def privacy():
     return render_template('privacy.html')
